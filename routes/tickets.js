3d75b2d970d46833e7aa52eb32d799ee6e6def0c
Baptiste LAUR <baptiste.laur@gmail.com>
2022-05-18 16:20:04 +0200

    Update tickets.js

--- routes/tickets.js
+++ routes/tickets.js
@@ -20,4 +20,35 @@ router.get("/", async function (req, res) {
   }
 });
 
+// GET tickets status
+router.get("/isWon/:id", async function (req, res) {
+  try {
+    var con = await dbConnection();
+    await con.query("SELECT * FROM tickets WHERE id = ?", [req.params.id], (error, results, fields) => {
+      con.release();
+      let isWon = true
+      let finished = true
+
+      for (const data of results) { 
+        switch (data.result) {
+          case "0":
+            isWon = false
+            break;
+          case null:
+            finished = false
+            break;
+        }
+      }
+
+      if (!finished) { isWon = false }
+
+      res.end(JSON.stringify({"isWon": isWon, 
+                              "finished": finished}
+                            ));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
